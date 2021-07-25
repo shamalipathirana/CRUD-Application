3b0371830fa1e25e3c8d27d2e35ba35eda59b6d1
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-25 14:45:19 +0530

    Add form to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="App">
       <h1>CRUD Application</h1>
+      <div className="form">
+        <label>Movie Name:</label>
+        <input type="text" name="movieName"/>
+        <label>Review:</label>
+        <input type="text" name="movieName"/>
+
+      </div>
+
     </div>
   );
 }
