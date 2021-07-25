f72de3b593a1bad64956bfe4c70d695494a76b5f
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-25 21:48:33 +0530

    Created database connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mysql=require("mysql");
 
+const db = mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"cruddatabase",
+});
+
+app.get("/", (req ,res) => {
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('incption','good movie');";
+    db.query(sqlInsert,(err,result)=>{
+        res.send("hello sha");
+    })
+});
 app.listen(3001, ()=>{
     console.log("running on port 3001");
 });
